82c7f7fea1821ed73e48fba463ef6ab5199379cd
Sal <salerno.salvatore1997@gmail.com>
2024-07-18 00:23:35 +0200

    made a more secure matrix implementation

--- src/parallel_fastflow.cpp
+++ src/parallel_fastflow.cpp
@@ -20,37 +20,75 @@
 #include<ff/farm.hpp>
 #include<thread>
 
-using u64 = uint64_t;
 
-void print_matrix(std::vector<double>& mtx, u64 row_length) {
-    std::cout<<std::endl<<std::endl;
-    for(u64 i=0; i < row_length; i++) {
-        for(u64 j=0; j < row_length; j++) {
-            std::cout<<mtx[i*row_length + j]<<" ";
-        }
-        std::cout<<std::endl;
+// Struct for our Square Matrix
+// The matrix is initialized as a matrix of size nxn s.t.
+// ∀m in [0, n].mtx[m][m] = (m + 1) / n.
+// All other elements in the matrix are set to 0.
+struct SquareMtx {
+    //Constructor
+    explicit SquareMtx(int row_length)
+        :data(row_length*row_length, 0), row_length(row_length)
+    {
+        for(int m{0}; m < row_length; ++m)
+            data[ (m * row_length) + m ] = static_cast<double>(m + 1)/ static_cast<double>(row_length) ;
     }
-}
+
+    /**
+        * @brief Return the real index where the cell mtx[row][col]
+        *        is stored.
+        * @param[in] row = the row index of the cell.
+        * @param[in] col = the column index of the cell.
+    */
+    [[nodiscard]] int GetIndex(int row, int col) const {
+        return (row * row_length) + col;
+    }
+
+    /**
+        * @brief Return the value at position mtx[row][col]
+        * @param[in] row = the row index of the cell.
+        * @param[in] col = the column index of the cell.
+    */
+    double GetValue(int row, int col) {
+        return data[GetIndex(row, col)];
+    }
+
+    /**
+        * @brief Does mtx[row][col] = val
+        * @param[in] row = the row index of the cell.
+        * @param[in] col = the column index of the cell.
+        * @param[in] val = the value to set in mtx[row][col]
+    */
+    void SetValue(int row, int col, double val) {
+        data[GetIndex(row, col)] = val;
+    }
+    /**
+        * @brief Prints the matrix
+    */
+    void PrintMtx(){
+        std::cout << "\n";
+        for(int i = 0; i < row_length; ++i) {
+            for(int j = 0; j < row_length; ++j)
+                std::cout << data[GetIndex(i, j)] << " ";
+            std::cout << "\n";
+        }
+    }
+
+    // Parameters
+    std::vector<double> data;
+    int row_length;
+};
 
 /**
  * @brief Represent a task that the Emitter will give to one of
  *        its Workers.
- *
- * This struct contains:
- * - mtx = a reference to the matrix.
- * - left_range = the first element of the chunk.
- * - right_range = the last element of the chunk.
- * - num_diag = the upper diagonal where the chunk is.
- *              Assume that -1 is the num_diag for the
- *              major diagonal.
  */
 
 struct Task{
-    std::vector<double>& mtx;   // Reference to the matrix
-    u64 left_range;           // First element of the diagonal to compute
-    u64 right_range;          // Last element of the diagonal to compute
-    u64 num_diag;             // The upper diagonal where the chunk is
-    u64 row_length;           // The length of a row (or column) of mtx
+    SquareMtx& mtx;   // Reference to the matrix
+    int left_range;           // First element of the diagonal to compute
+    int right_range;          // Last element of the diagonal to compute
+    int num_diag;             // The upper diagonal where the chunk is
 };
 
 /**
@@ -62,19 +100,28 @@ struct Task{
  *        computed the Emitter sends Tasks relative to the i+1
  *        diagonal.
  * Return Type: Task, it gives one to a Worker
- * Receiver Type: u64, it gets one from a Worker.
+ * Receiver Type: int, it gets one from a Worker.
  *                It represents the number of computed elements.
  */
-struct Emitter: ff::ff_monode_t<u64, Task> {
+struct Emitter: ff::ff_monode_t<int, Task> {
     // Constructor
-    explicit Emitter(std::vector<double>& mtx, u64 row_length,
-                     u64 num_workers)
-        :mtx(mtx), row_length(row_length),num_workers(num_workers)
-        {
-        curr_diag = 0;  // We assume the major diagonal is 0
-        chunk_size = 0;
+    explicit Emitter(SquareMtx& mtx, const int num_workers)
+        :mtx(mtx), num_workers(num_workers)
+    {PrepareNextDiagonal();}
+
+    /**
+        * @brief This method prepare all parameters of the Emitter for computing
+        *        the elements of the next matrix.
+    */
+    void PrepareNextDiagonal() {
         send_tasks = true;
-        curr_diag_length = 0;
+        curr_diag++;
+        curr_diag_length = mtx.row_length - curr_diag;
+
+        // Setting Chunk_Size
+        chunk_size = (std::ceil(static_cast<float>(curr_diag_length) / num_workers));
+        if (chunk_size > 64)    // Like Super Mario hihihi
+            chunk_size = 64;
     }
 
     /**
@@ -84,56 +131,42 @@ struct Emitter: ff::ff_monode_t<u64, Task> {
      * @param [in] done_wrk = contains the number of elements computed
      *                        by a Worker.
     */
-    Task* svc(u64* done_wrk) {
+    Task* svc(int* done_wrk) {
         if (done_wrk != nullptr) { // A Worker as returned a message
             curr_diag_length -= *done_wrk;
-            delete(done_wrk);
 
             if(curr_diag_length <= 0) { // All elements in curr_diag
                                         // have been computed. Starting
                                         // works on the next diagonal.
-                if(curr_diag >= row_length - 1) {   // All diagonals have been
-                                                // computed. Closing the farm.
+                PrepareNextDiagonal();
+                if(curr_diag >= mtx.row_length) {   // All diagonals have been
+                                                    // computed. Closing the farm.
                     eosnotify();
                     return EOS;
                 }
-                send_tasks = true;
             }
         }
-
         if(send_tasks) {     // Sending Task for next diagonal
-            // Setting Parameters
-            send_tasks = false;
-            curr_diag++;
-            curr_diag_length = row_length - curr_diag;
-
-            // Setting Chunk_Size
-            chunk_size = (std::ceil(static_cast<float>(curr_diag_length) / num_workers));
-            if (chunk_size > 64)    // Like Super Mario hihihi
-                chunk_size = 64;
-
-            // Sending Tasks
-            for(u64 i=0; i < curr_diag_length; i += chunk_size) {   // the first element is 0!!!
+            for(int i=0; i < curr_diag_length; i += chunk_size) {   // the first element is 0!!!
                 auto* task = new Task{mtx,
                                       i,
                                       i+chunk_size-1,
-                                      curr_diag,
-                                      row_length};
+                                      curr_diag};
                 ff_send_out(task);
             }
+            send_tasks = false;
             //TODO fai fare un task all'Emitter per non fargli fare attesa attiva
         }
         return GO_ON;
     }
 
     // Parameters
-    std::vector<double>& mtx;   // Reference to the matrix to compute.
-    u64 row_length;          // The Lenght of a row (or column) of mtx.
-    u64 curr_diag;           // The current diagonal.
-    u64 curr_diag_length;    // Number of elements of the curr. diagonal.
-    u64 chunk_size;          // Number of elements per Worker.
-    u64 num_workers;         // Number of workers employed by the farm.
-    bool send_tasks;            // Tells if we have to send tasks to Workers.
+    SquareMtx& mtx;   // Reference to the matrix to compute.
+    int curr_diag{0};           // The current diagonal.
+    int curr_diag_length{0};    // Number of elements of the curr. diagonal.
+    int chunk_size{0};          // Number of elements per Worker.
+    int num_workers{0};         // Number of workers employed by the farm.
+    bool send_tasks{false};            // Tells if we have to send tasks to Workers.
 };
 
 
@@ -146,44 +179,58 @@ struct Emitter: ff::ff_monode_t<u64, Task> {
  * Receiver Type: Task*, it gets one from the Emitter.
  *                It represents the number of computed elements.
  */
-struct Worker: ff::ff_node_t<Task, u64> {
-    u64* svc(Task* t) {
+struct Worker: ff::ff_node_t<Task, int> {
 
-        // Checking that right_range isn't out of bounds
-        if(t->right_range > (t->row_length - t->num_diag) - 1)
-            t->right_range = (t->row_length - t->num_diag) - 1;
+    int* svc(Task* t) {
+
+        // Checking that the last element isn't out of bounds
+        [](int& range, int limit) {
+            if(range > limit)
+                range = limit;
+        }(t->right_range, (t->mtx.row_length - t->num_diag) - 1);
 
         // Setting parameters
-        u64 num_elements = t->right_range - t->left_range + 1;
-        double temp{1.0};
+        const int num_elements = t->right_range - t->left_range + 1;
+        double temp{0.0};
 
         // Starting the computation of elements
-        for(u64 k = 0; k < num_elements; ++k) {
-            u64 i = t->left_range + k; u64 j = t->num_diag + k + t->left_range;
+        for(int k = 0; k < num_elements; ++k) {
+            int i = t->left_range + k; int j = t->num_diag + k + t->left_range;
 
             //Setting parameters for DotProduct
-            const u64 idx_vecr_row = i;  u64 idx_vecr_col = i;      // Indexes for the first vector
-            const u64 idx_vecc_row = j;  u64 idx_vecc_col = i + 1;  // Indexes for the second vector
+            const int idx_vecr_row = i;  int idx_vecr_col = i;      // Indexes for the first vector
+            const int idx_vecc_row = j;  int idx_vecc_col = i + 1;  // Indexes for the second vector
                                                                     // We do not work with the column vector
                                                                     // but with a row in the lower triangular
                                                                     // that contains the same elements
                                                                     // Computing the DotProduct
-            for(u64 elem_vec=0; elem_vec < j-i ; ++elem_vec) {
-                temp += t->mtx[idx_vecr_row * t->row_length + idx_vecr_col] *
-                                  t->mtx[idx_vecc_row * t->row_length + idx_vecc_col];
-                idx_vecr_col++;
-                idx_vecc_col++;
+            for(int elem=0; elem < j-i ; ++elem) {
+                temp += t->mtx.GetValue(idx_vecr_row, idx_vecr_col + elem) * t->mtx.GetValue(idx_vecc_row, idx_vecc_col + elem);
             }
 
             // Storing the result
-
-            t->mtx[i * t->row_length + j] = t->num_diag;//std::cbrt(temp);
-            t->mtx[j * t->row_length + i] = t->num_diag;//std::cbrt(temp);
+            t->mtx.SetValue(i, j, std::cbrt(temp));
+            t->mtx.SetValue(j, i, std::cbrt(temp));
+            temp = 0.0; // Resetting temp
         }
-        delete(t);
-        return new u64(num_elements);
+        return new int(num_elements);
     }
 };
+
+/**
+ * @brief Determines the number of workers of the farm by determining
+ *        the affinity of the hardware
+ * @param[in] def = default value
+ */
+int DetermineWorkers(int def) {
+    if(std::thread::hardware_concurrency() > 0)
+        return std::thread::hardware_concurrency() -1; // One thread is for the Emitter
+    return def;
+}
+
+int DetermineLength() {
+
+}
 
 /**
  * @brief A Parallel Wavefront Computation using the library FastFlow.
@@ -199,47 +246,42 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     if (std::stoll(argv[1]) <= 0) {
-        std::cerr << "Invalid lenght! The lenght must be a positive u64eger != 0"
+        std::cerr << "Invalid lenght! The lenght must be a positive integer != 0"
                   << std::endl;
         return -1;
     }
-    const u64 n{static_cast<u64>(std::stoll(argv[1]))};
+    const int n{static_cast<int>(std::stoll(argv[1]))};
+    // Initialize Matrix
+    SquareMtx mtx(n);
+    mtx.PrintMtx();
 
     // Setting number of workers
-    u64 num_workers{6};
-    if(std::thread::hardware_concurrency() > 0)
-        num_workers = std::thread::hardware_concurrency() -1; // One thread is for the Emitter
-
-    // Initialize mtx as a matrix of size nxn s.t.
-    // ∀m in [0, n].mtx[m][m] = (m + 1) / n.
-    // All other elements in the matrix are set to 0.
-    std::vector<double> mtx{static_cast<double>(n * n), 0};
-    for(u64 m{0}; m < n; ++m)
-        mtx[ (m * n) + m ] = static_cast<double>(m + 1)/ static_cast<double>(n) ;
-    //print_matrix(mtx,n);   //TODO REMOVE CALL
+    int num_workers = DetermineWorkers(6);
 
     // Creating the Farm
+    Emitter emt{mtx, num_workers};
     ff::ff_Farm<> farm(
         [&]() {
             std::vector<std::unique_ptr<ff::ff_node>> workers;
             for(size_t i=0; i < num_workers; ++i)
                 workers.push_back(std::make_unique<Worker>());
             return workers;
-        }()
-    );
-    Emitter emt{mtx, n, num_workers};
-    farm.add_emitter(emt);
+        }(),
+        emt);
     farm.remove_collector();
     farm.wrap_around();
 
     // Starting the farm
-    ff::ffTime(ff::START_TIME);
+    const auto start = std::chrono::steady_clock::now();
     if(farm.run_and_wait_end()<0) {
         std::cerr<<"While running farm";
         return -1;
     }
-    ff::ffTime(ff::STOP_TIME);
-    std::cout << "Time taken for parallel version: " << ff::ffTime(ff::GET_TIME) << " milliseconds" << std::endl;
+    const auto end = std::chrono::steady_clock::now();
+    const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Time taken for parallel version: " << duration.count() << " milliseconds" << std::endl;
+
+    mtx.PrintMtx();
     return 0;
 }
 
--- src/sequential.cpp
+++ src/sequential.cpp
@@ -1,5 +1,5 @@
 /**
-* @file sequential.cpp
+ * @file sequential.cpp
  * @brief This code is a sequential implementation of the Wavefront computation.
  * @details Sequential implementation of the Wavefront computation.
  *          It initialize a matrix of dimension NxN (parameter given as input).
@@ -10,9 +10,68 @@
  * @author Salvatore Salerno
  */
 
-#include<vector>
-#include<iostream>
-#include<cmath>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+
+// Struct for our Square Matrix
+// The matrix is initialized as a matrix of size nxn s.t.
+// ∀m in [0, n].mtx[m][m] = (m + 1) / n.
+// All other elements in the matrix are set to 0.
+struct SquareMtx {
+    //Constructor
+    explicit SquareMtx(int row_length)
+        :data(row_length*row_length, 0), row_length(row_length)
+    {
+        for(int m{0}; m < row_length; ++m)
+            data[ (m * row_length) + m ] = static_cast<double>(m + 1)/ static_cast<double>(row_length) ;
+    }
+
+    /**
+        * @brief Return the real index where the cell mtx[row][col]
+        *        is stored.
+        * @param[in] row = the row index of the cell.
+        * @param[in] col = the column index of the cell.
+    */
+    [[nodiscard]] int GetIndex(int row, int col) const {
+        return (row * row_length) + col;
+    }
+
+    /**
+        * @brief Return the value at position mtx[row][col]
+        * @param[in] row = the row index of the cell.
+        * @param[in] col = the column index of the cell.
+    */
+    double GetValue(int row, int col) {
+        return data[GetIndex(row, col)];
+    }
+
+    /**
+        * @brief Does mtx[row][col] = val
+        * @param[in] row = the row index of the cell.
+        * @param[in] col = the column index of the cell.
+        * @param[in] val = the value to set in mtx[row][col]
+    */
+    void SetValue(int row, int col, double val) {
+        data[GetIndex(row, col)] = val;
+    }
+    /**
+        * @brief Prints the matrix
+    */
+    void PrintMtx(){
+        std::cout << "\n";
+        for(int i = 0; i < row_length; ++i) {
+            for(int j = 0; j < row_length; ++j)
+                std::cout << data[GetIndex(i, j)] << " ";
+            std::cout << "\n";
+        }
+    }
+
+    // Parameters
+    std::vector<double> data;
+    int row_length;
+};
 
 /**
  * @brief Apply the WaveFront Computation.
@@ -22,38 +81,36 @@
  * @param[in] n = the length of a row (or a column) of the matrix.
  * @param[out] mtx = the reference to the matrix to initialize.
  */
-void ComputeMatrix(const uint64_t n, std::vector<double>& mtx) {
- double temp{0.0}; // Temporary value used to store
-                   // the DotProduct computation.
+void ComputeMatrix(const int n, SquareMtx& mtx) {
+    double temp{0.0}; // Temporary value used to store
+                      // the DotProduct computations.
 
- for(uint64_t iter{0}; iter < n; ++iter) {
-  // Setting parameters for current iteration
-  uint64_t i = 0; uint64_t j = iter + 1;
+    for (int iter{0}; iter < n; ++iter) { // iter = number of the current diagonal
+        // Setting parameters for current iteration
+        int i = 0;
+        int j = iter + 1;
 
-  // Computing the elements of the current diagonal
-  while( j < n ) {
-   // Setting parameters for DotProduct
-   const uint64_t idx_vecr_row = i;   uint64_t idx_vecr_col = i;     // Indexes for the first vector
-   const uint64_t idx_vecc_row = j;  uint64_t idx_vecc_col = i + 1;  // Indexes for the second vector
-                                                                     // We do not work with the column vector
-                                                                     // but with a row in the lower triangular
-                                                                     // that contains the same elements
+        // Computing the elements of the current diagonal
+        while (j < n) {
+            // Setting parameters for DotProduct
+            const int idx_vecr_row = i; int idx_vecr_col = i; // Indexes for the first vector
+            const int idx_vecc_row = j; int idx_vecc_col = i + 1; // Indexes for the second vector
+                                                                  // We do not work with the column vector
+                                                                  // but with a row in the lower triangular
+                                                                  // that contains the same elements
 
-   // Computing the DotProduct
-   for(uint64_t elem_vec{0}; elem_vec < j-i ; ++elem_vec) {
-    temp += mtx[idx_vecr_row * n + idx_vecr_col] *
-                      mtx[idx_vecc_row * n + idx_vecc_col];
-    idx_vecr_col++;
-    idx_vecc_col++;
-   }
-   ++i; ++j;
-  }
-
-  // Applying the cube root
-  mtx[i * n + j] = std::cbrt(temp);
-  mtx[j * n + i] = temp;  // storing the element in the lower triangular
-  temp = 0.0;
- }
+            // Computing the DotProduct
+            for (int elem{0}; elem < j - i; ++elem) {
+                temp += mtx.GetValue(idx_vecr_row, idx_vecr_col + elem) * mtx.GetValue(idx_vecc_row, idx_vecc_col + elem);
+            }
+            // Applying the cube root
+            mtx.SetValue(i, j, std::cbrt(temp));
+            mtx.SetValue(j, i, std::cbrt(temp));
+            temp = 0.0;
+            ++i;
+            ++j;
+        }
+    }
 }
 
 /**
@@ -61,39 +118,31 @@ void ComputeMatrix(const uint64_t n, std::vector<double>& mtx) {
  * @param[in] argv[1] is the lenght a row (or a column) of the matrix
  * @exception argc < 2
  */
-int main(int argc, char* argv[]) {
- if(argc!=2) {
-  std::cerr << "You must pass as argument the lenght of the square matrix to generate"
-            << std::endl;
-  return -1;
- }
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "You must pass as argument the lenght of the square matrix to generate" << std::endl;
+        return -1;
+    }
 
- if (std::stoll(argv[1]) <= 0) {
-  std::cerr << "Invalid lenght! The lenght must be a positive integer != 0"
-            << std::endl;
-  return -1;
- }
- const auto n{static_cast<int>(std::stoll(argv[1]))};
+    if (std::stoll(argv[1]) <= 0) {
+        std::cerr << "Invalid lenght! The lenght must be a positive integer != 0" << std::endl;
+        return -1;
+    }
+    const auto n{static_cast<int>(std::stoll(argv[1]))};
 
 
+    // Initialize mtx
+    SquareMtx mtx(n);
 
- // Initialize mtx as a matrix of size nxn s.t.
- // ∀m in [0, n].mtx[m][m] = (m + 1) / n.
- // All other elements in the matrix are set to 0.
- std::vector<double> mtx{static_cast<double>(n*n), 0};
- for(int m{0}; m < n; ++m)
-     mtx[ (m * n) + m ] = static_cast<double>(m + 1)/ static_cast<double>(n) ;
+    // Beginning WaveFront Pattern
+    const auto start = std::chrono::steady_clock::now();
 
- // Beginning WaveFront Pattern
- const auto start = std::chrono::steady_clock::now();
+    if (n > 1) // If we have a 1x1 matrix, then we
+               // do not need to apply the computation
+        ComputeMatrix(n, mtx);
 
- if(n > 1) // If we have a 1x1 matrix, then we
-           // do not need to apply the computation
-   ComputeMatrix(n, mtx);
-
- const auto end = std::chrono::steady_clock::now();
- const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
- std::cout << "Time taken for sequential version: " << duration.count() << " milliseconds" << std::endl;
-
- return 0;
+    const auto end = std::chrono::steady_clock::now();
+    const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Time taken for sequential version: " << duration.count() << " milliseconds" << std::endl;
+    return 0;
 }
