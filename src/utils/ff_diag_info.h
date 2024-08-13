ca20d49152254f6a4c287e34bd0fd62f42ad2b6e
Sal <salerno.salvatore1997@gmail.com>
2024-08-13 19:58:07 +0200

    now send_info has become diag_info. Contains ONLY information regarding the diagonal

--- src/utils/ff_diag_info.h
+++ src/utils/ff_diag_info.h
@@ -2,37 +2,36 @@
 // Created by Salvatore Salerno on 11/08/24. TODO
 //
 
-#ifndef FF_SEND_INFO_H
-#define FF_SEND_INFO_H
+#ifndef FF_DIAG_INFO_H
+#define FF_DIAG_INFO_H
 
 #include "constants.h"
 
 
 /**
- * @brief Contains informations for the Emitter regarding
- *        when and what diagonal to send to the Workers
+ * @brief Contains informations about the current diagonal to compute
+ *        when and what diagonal to send to the Workers.
+ *        ONLY THE EMITTER MODIFIES IT, THE WORKER ONLY READ IT
  */
-struct SendInfo {
-    explicit SendInfo(const u64 base_lenght, const u64 num_workers)
-    : diag_length(base_lenght), num_workers(num_workers){PrepareNextDiagonal();}
+struct DiagInfo {
+    explicit DiagInfo(const u64 base_lenght, const u64 num_workers)
+    : num_workers(num_workers), length(base_lenght) {PrepareNextDiagonal();}
 
     /**
     * @brief This method prepare all parameters of the Emitter for computing
     *        the elements of the next diagonal of the matrix.
     */
     void PrepareNextDiagonal() {
-        send_tasks = true;
-        diag++;
-        diag_length--;
-        active_workers = 0;
-        chunk_size = static_cast<u64>(std::ceil(diag_length / num_workers));
+        num++;
+        length--;
+        chunk_size = static_cast<u64>(std::ceil(length / num_workers));
     }
 
     // PARAMETERS
-    u8 num_workers;
+    const u8 num_workers;
     u64 chunk_size{0};
-    u64 diag{0};                // The current diagonal.
-    u64 diag_length{0};         // Number of elements of the curr. diagonal.
+    u64 num{0};                // The current diagonal.
+    u64 length{0};         // Number of elements of the curr. diagonal.
 };
 
-#endif //FF_SEND_INFO_H
+#endif //FF_DIAG_INFO_H
