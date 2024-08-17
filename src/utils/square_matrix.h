f0bb3d9d6f813e6fc897616c67fe86ea1f732d49
Sal <salerno.salvatore1997@gmail.com>
2024-08-17 17:49:15 +0200

    Rewritter description of methods, now it's more readable

--- src/utils/square_matrix.h
+++ src/utils/square_matrix.h
@@ -1,5 +1,5 @@
 /**
-* @file square_matrix.h
+ * @file square_matrix.h
  * @brief The Square matrix class used throught the project.
  * @details TODO ADD A DESCRIPTION
  * @author Salvatore Salerno
@@ -8,76 +8,79 @@
 #ifndef SQUARE_MATRIX_H
 #define SQUARE_MATRIX_H
 
-#include "elem_info.h"
 #include "constants.h"
+#include "elem_info.h"
 
 // Struct for our Square Matrix
 // The matrix is initialized as a matrix of size nxn s.t.
 // ∀m in [0, n].mtx[m][m] = (m + 1) / n.
 // All other elements in the matrix are set to 0.
+/**
+ * @brief Represents a Square Matrix, storing all the data in a single std::vector<double>.
+ */
 struct SquareMtx {
-    //Constructor
-    SquareMtx()= default;
-
-    explicit SquareMtx(const u64& length)
-        :length(length) {
-        InitializeMatrix();
-    }
+    /**
+     * @brief default constructor, creates a uninitialized matrix.
+     */
+    SquareMtx() = default;
 
     /**
-        * @brief allocated the data buffer with length elements
-    */
+     * @brief Creates an initialized matrix
+     * @param[in] length = sets the lenght of the matrix.
+     */
+    explicit SquareMtx(const u64 &length) : length(length) { InitializeMatrix(); }
+
+    /**
+     * @brief allocated the data buffer with length elements
+     */
     void AllocateData() {
-        if(length == 0) {
+        if (length == 0) {
             std::cerr << "ERROR!!!! Matrix not initialized" << std::endl;
         }
-        data = new std::vector<double>(length*length, 0.0);
+        data = new std::vector<double>(length * length, 0.0);
     }
 
     /**
-        * @brief Return the real index where the cell mtx[row][col]
-        *        is stored.
-        * @param[in] row = the row index of the cell.
-        * @param[in] col = the column index of the cell.
-    */
-    [[nodiscard]] u64 GetIndex(const u64 row, const u64 col) const {
-        return (row * length) + col;
-    }
+     * @brief Return the real index where the cell mtx[row][col]
+     *        is stored.
+     * @param[in] row = the row index of the cell.
+     * @param[in] col = the column index of the cell.
+     */
+    [[nodiscard]] u64 GetIndex(const u64 row, const u64 col) const { return (row * length) + col; }
 
     /**
-        * @brief Return the real index where the cell mtx[row][col]
-        *        is stored.
-        * @param[in] elem = an ElemInfo object.
-    */
-    [[nodiscard]] u64 GetIndex(const ElemInfo elem) const {
-        return (elem.row * length) + elem.col;
-    }
-
-    /**
-        * @brief Initialize the matrix with the length it has.
-    */
+     * @brief Initialize the matrix with the length it has s.t.
+     * ∀m in [0, n].mtx[m][m] = (m + 1) / n.
+     * All other elements in the matrix are set to 0.
+     */
     void InitializeMatrix() {
         AllocateData();
-        for(u64 m = 0; m < length; ++m)
-            (*data)[ (m * length) + m ] = static_cast<double>(m + 1)/ static_cast<double>(length) ;
+        for (u64 m = 0; m < length; ++m)
+            (*data)[(m * length) + m] = static_cast<double>(m + 1) / static_cast<double>(length);
     }
 
     /**
-    * @brief Initialize the matrix with the length it has.
-*/
-    void InitializeMatrix(u64 length) {
+     * @brief Initialize the matrix with the given length
+     * @param[in] new_length = the length of the matrix
+     */
+    void InitializeMatrix(const u64 new_length) {
+        if (length != 0) {
+            std::cerr << "ERROR! Matrix already initialized";
+            return;
+        }
+        length = new_length;
         AllocateData();
-        for(u64 m{0}; m < length; ++m)
-            (*data)[ (m * length) + m ] = static_cast<double>(m + 1)/ static_cast<double>(length) ;
+        for (u64 m{0}; m < length; ++m)
+            (*data)[(m * length) + m] = static_cast<double>(m + 1) / static_cast<double>(length);
     }
 
     /**
-        * @brief Return the value at position mtx[row][col]
-        * @param[in] row = the row index of the cell.
-        * @param[in] col = the column index of the cell.
-    */
+     * @brief Return the value at position mtx[row][col]
+     * @param[in] row = the row index of the cell.
+     * @param[in] col = the column index of the cell.
+     */
     [[nodiscard]] double GetValue(const u64 row, const u64 col) const {
-        if(length == 0) {
+        if (length == 0) {
             std::cerr << "ERROR!!!! Matrix not initialized" << std::endl;
             return -1.0;
         }
@@ -85,13 +88,13 @@ struct SquareMtx {
     }
 
     /**
-        * @brief Does mtx[row][col] = val
-        * @param[in] row = the row index of the cell.
-        * @param[in] col = the column index of the cell.
-        * @param[in] val = the value to set in mtx[row][col]
-    */
+     * @brief Does mtx[row][col] = val
+     * @param[in] row = the row index of the cell.
+     * @param[in] col = the column index of the cell.
+     * @param[in] val = the value to set in mtx[row][col]
+     */
     void SetValue(u64 row, const u64 col, const double val) const {
-        if(length == 0) {
+        if (length == 0) {
             std::cerr << "ERROR!!!! Matrix not initialized" << std::endl;
             return;
         }
@@ -99,10 +102,10 @@ struct SquareMtx {
     }
 
     /**
-        * @brief Prints the whole matrix
-    */
-    void PrintMtx() const{
-        if(length == 0) {
+     * @brief Prints the whole matrix
+     */
+    void PrintMtx() const {
+        if (length == 0) {
             std::cerr << "ERROR!!!! Matrix not initialized" << std::endl;
             return;
         }
@@ -112,30 +115,15 @@ struct SquareMtx {
         }
 
         std::cout << "\n";
-        for(u64 i = 0; i < length; ++i) {
-            for(u64 j = 0; j < length; ++j)
+        for (u64 i = 0; i < length; ++i) {
+            for (u64 j = 0; j < length; ++j)
                 std::cout << (*data)[GetIndex(i, j)] << " ";
             std::cout << "\n";
         }
     }
 
-    /**
-        * @brief DEBUG function, fills the matrix with
-        *        s.t. ∀ mtx[i][j] = j
-    */
-    void FillMatrix() const {
-        if(length == 0) {
-            std::cerr << "ERROR!!!! Matrix not initialized" << std::endl;
-            return;
-        }
-
-        for(u64 i = 0; i < length; ++i)
-            for (u64 j = 0; j < length; ++j)
-                SetValue(i, j, static_cast<double>(j));
-    }
-
     // Parameters
-    std::vector<double>* data{nullptr};
+    std::vector<double> *data{nullptr};
     u64 length{0};
 };
-#endif //SQUARE_MATRIX_H
+#endif // SQUARE_MATRIX_H
