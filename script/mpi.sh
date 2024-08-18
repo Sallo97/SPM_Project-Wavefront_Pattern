45b9dbbf1423e6246b37eefd17f0bda9784bede6
Sal <salerno.salvatore1997@gmail.com>
2024-08-18 12:28:57 +0200

    changed where to find the executable

--- script/mpi.sh
+++ script/mpi.sh
@@ -6,10 +6,9 @@
 #SBATCH -e ../results/%j.err
 #SBATCH --time=00:30:00
 #SBATCH --ntasks-per-node=1
-PROBLEM_SIZE=4098
+PROBLEM_SIZE=4096
 
 srun /bin/hostname
 
 # Run the MPI program with the specified problem size
-
-mpirun -map-by ppr:1:node --report-bindings ../out/parallel_mpi $PROBLEM_SIZE
+mpirun -map-by ppr:1:node --report-bindings ../build/src/pasrallel_mpi $PROBLEM_SIZE
