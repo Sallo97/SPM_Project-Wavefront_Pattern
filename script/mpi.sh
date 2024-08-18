d833788395eb380472fe491b624bb49cfe04e723
Sal <salerno.salvatore1997@gmail.com>
2024-08-18 11:00:52 +0200

    added test script for mpi

---
+++ script/mpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Set the variables from command-line arguments
+#SBATCH --partition=normal
+#SBATCH --job-name=parallel_mpi
+#SBATCH -o ../results/%j.log
+#SBATCH -e ../results/%j.err
+#SBATCH --time=00:30:00
+#SBATCH --ntasks-per-node=1
+PROBLEM_SIZE=4098
+
+srun /bin/hostname
+
+# Run the MPI program with the specified problem size
+
+mpirun -map-by ppr:1:node --report-bindings ../out/parallel_mpi $PROBLEM_SIZE
