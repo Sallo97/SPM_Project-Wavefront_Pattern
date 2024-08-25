d0c7249c95ad4ca112611fc5a2327279805a5e96
Sal <salerno.salvatore1997@gmail.com>
2024-08-25 17:35:43 +0200

    fixed the tests

--- script/mpi/mpi_8_process.sh
+++ script/mpi/mpi_8_process.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=mpi_4_measurements
+#SBATCH --job-name=mpi_8_measurements
 #SBATCH --output=../../results/mpi/mpi_4_nodes%A_%a.out
 #SBATCH --error=../../results/mpi/error_mpi_4_nodes%A_%a.err
 #SBATCH --time=00:30:00 #30 min (hrs:min:sec)
-#SBATCH --nodes=4
+#SBATCH --nodes=8
 
 # Number of execution of the program
 num_execution=5
@@ -11,7 +11,7 @@ start_val=2048 #from 2048 to 32'768
 for i in $(seq 0 $((num_execution-1)))
 do
   arg=$((start_val * (2 ** i)))
-  echo "MPI execution 1 task per node, 4 nodes, with argument: $arg"
+  echo "MPI execution 1 task per node, 8 nodes, with argument: $arg"
   # shellcheck disable=SC2086
-  mpirun -np 4 --map-by ppr:1:node ../../build/src/parallel_mpi $arg
+  mpirun -np 8 --map-by ppr:1:node ../../build/src/parallel_mpi $arg
 done
