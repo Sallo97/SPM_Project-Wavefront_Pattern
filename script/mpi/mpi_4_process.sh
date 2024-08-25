9359c6a6957d6f0bb04fc2c43a5dc774ebffaeff
Sal <salerno.salvatore1997@gmail.com>
2024-08-25 17:45:18 +0200

    make case where I give two task for each node

--- script/mpi/mpi_4_process.sh
+++ script/mpi/mpi_4_process.sh
@@ -4,6 +4,7 @@
 #SBATCH --error=../../results/mpi/error_mpi_4_nodes%A_%a.err
 #SBATCH --time=00:30:00 #30 min (hrs:min:sec)
 #SBATCH --nodes=4
+#SBATCH --ntasks-per-node=2
 
 # Number of execution of the program
 num_execution=5
@@ -12,6 +13,15 @@ for i in $(seq 0 $((num_execution-1)))
 do
   arg=$((start_val * (2 ** i)))
   echo "MPI execution 1 task per node, 4 nodes, with argument: $arg"
-  # shellcheck disable=SC2086
   mpirun -np 4 --map-by ppr:1:node ../../build/src/parallel_mpi $arg
+done
+
+# Number of execution of the program
+num_execution=5
+start_val=2048 #from 64 to 32'768
+for i in $(seq 0 $((num_execution-1)))
+do
+  arg=$((start_val * (2 ** i)))
+  echo "MPI execution 2 tasks per node, 4 nodes (total 8 processes), with argument: $arg"
+  mpirun -np 8 --map-by ppr:2:node ../../build/src/parallel_mpi $arg
 done
--- script/mpi/mpi_8_process.sh
+++ script/mpi/mpi_8_process.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=mpi_8_measurements
-#SBATCH --output=../../results/mpi/mpi_4_nodes%A_%a.out
-#SBATCH --error=../../results/mpi/error_mpi_4_nodes%A_%a.err
+#SBATCH --output=../../results/mpi/mpi_8_nodes%A_%a.out
+#SBATCH --error=../../results/mpi/error_mpi_8_nodes%A_%a.err
 #SBATCH --time=00:30:00 #30 min (hrs:min:sec)
 #SBATCH --nodes=8
+#SBATCH --ntasks-per-node=2
 
 # Number of execution of the program
 num_execution=5
@@ -14,4 +15,14 @@ do
   echo "MPI execution 1 task per node, 8 nodes, with argument: $arg"
   # shellcheck disable=SC2086
   mpirun -np 8 --map-by ppr:1:node ../../build/src/parallel_mpi $arg
+done
+
+# Number of execution of the program
+num_execution=5
+start_val=2048 #from 2048 to 32'768
+for i in $(seq 0 $((num_execution-1)))
+do
+  arg=$((start_val * (2 ** i)))
+  echo "MPI execution 2 task per node, 8 nodes (total 16 processes), with argument: $arg"
+  mpirun -np 16 --map-by ppr:2:node ../../build/src/parallel_mpi $arg
 done
