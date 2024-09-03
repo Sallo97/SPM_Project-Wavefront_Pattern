94bf19557dfa32357e4bc977793f9e1284467ea8
Sal <salerno.salvatore1997@gmail.com>
2024-09-03 14:31:26 +0200

    created a script which handles all: compilation, log dirs and execute all tests

---
+++ script/general.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=all_together
+#SBATCH --time=03:00:00 #30 min (hrs:min:sec)
+#SBATCH --nodes=8
+
+# This script compiles the project, checks for log directories and executes all measurements scripts
+
+# Compile the project
+echo "Compiling the project..."
+chmod +x "./compile.sh"
+./compile.sh
+echo "OK, next"
+
+# Create directories where log files will be stored
+echo "Checking log directories..."
+chmod +rwx "./log_dirs.sh"
+./log_dirs.sh
+echo "OK, next"
+
+# List of measurement script
+scripts=(
+    "sequential_measurements.sh"
+#    "ff_static_measurements.sh"
+#    "ff_dynamic_measurements.sh"
+#    "./mpi/mpi_4_process.sh"
+#    "./mpi/mpi_8_process.sh"
+)
+
+# Loop through each script and execute it
+for script in "${scripts[@]}"; do
+    echo "Running $script..."
+
+    # Make sure the script is executable and execute the script
+    chmod +x "$script"
+    ./"$script"
+done
+
+echo "OK, all done successfully! BYE BYE!"
